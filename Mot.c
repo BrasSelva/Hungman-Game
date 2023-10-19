38f3bffcef5121287aa4f4f842fabe9f6b8c0d59
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2023-10-19 13:59:23 +0200

    Update Mot.c minimiliser le code

--- Mot.c
+++ Mot.c
@@ -5,6 +5,8 @@
 
 #define MAX_TRIES 7
 
+//Struture du jeu, on initie les variables
+
 struct HangmanGame {
     const char* motSecret;
     int longueurMot;
@@ -12,27 +14,15 @@ struct HangmanGame {
     int tries;
 };
 
-//void Replay() {
-//    char choix;
-//
-//    printf("Do you want to replay ? (O/N) : ");
-//    fflush(stdin);
-//    scanf_s(" %c", &choix);
-//
-//    if (choix == 'O' || choix == 'o') {
-//        // Si le joueur veut rejouer, appelez la fonction Game pour démarrer une nouvelle partie.
-//        Game();
-//    }
-//    else if (choix == 'N' || choix == 'n') {
-//        printf("Thank for playing ! Good bye.\n");
-//        //ExitApp(); // Vous pouvez également ajouter un appel à la fonction ExitApp pour quitter l'application ici.
-//    }
-//    else {
-//        // Réponse non valide, redemandez.
-//        printf("Answer incorrect. Please answer by Y(yes) or N(no)\n");
-//        Replay(); // Appel récursif pour redemander la réponse.
-//    }
-//}
+
+//Demarrer le jeu
+void Startup() {
+    printf (" Lancer le jeu ") ;
+}
+
+//Prototype de la fonction Replay
+void Replay(struct HangmanGame* game);
+
 
 void afficherMotPartiel(const struct HangmanGame* game) {
     for (int i = 0; i < game->longueurMot; i++) {
@@ -46,11 +36,14 @@ void afficherMotPartiel(const struct HangmanGame* game) {
     printf("\n");
 }
 
+// Cela permet de choisir un mot secret au hasard
+
 const char* choisirMotSecret(const char* motsSecrets[], int nombreDeMots) {
     srand((unsigned int)time(NULL));
     int indice = rand() % nombreDeMots;
     return motsSecrets[indice];
 }
+
 
 void initialiserPartie(struct HangmanGame* game, const char* motsSecrets[], int nombreDeMots) {
     game->motSecret = choisirMotSecret(motsSecrets, nombreDeMots);
@@ -59,12 +52,13 @@ void initialiserPartie(struct HangmanGame* game, const char* motsSecrets[], int 
     game->tries = 0;
 }
 
+//Quitter l'application
 void ExitApp() {
     printf("See you soon.\n");
+    exit(0);
 }
 
-void Game() {
-    struct HangmanGame game;
+void Game(struct HangmanGame* game) {
     const char* motsSecrets[] = {
         "programmation",
         "informatique",
@@ -74,22 +68,22 @@ void Game() {
     };
     const int nombreDeMots = sizeof(motsSecrets) / sizeof(motsSecrets[0]);
 
-    initialiserPartie(&game, motsSecrets, nombreDeMots);
+    initialiserPartie(game, motsSecrets, nombreDeMots);
 
-    while (game.tries < MAX_TRIES) {
+    while (game->tries < MAX_TRIES) {
         printf("\nSecret word: ");
-        afficherMotPartiel(&game);
-        printf("Guessed letter: %s\n", game.lettresDevinees);
-        printf("Tries remaining: %d\n", MAX_TRIES - game.tries);
+        afficherMotPartiel(game);
+        printf("Guessed letters: %s\n", game->lettresDevinees);
+        printf("Tries remaining: %d\n", MAX_TRIES - game->tries);
         printf("Press 2 to quit\n");
         printf("Enter a letter: ");
 
-        //Affiche le pendu au fur et à mesure des essais
-        if (game.tries > 0) {
+        // Affiche le pendu au fur et à mesure des essais
+        if (game->tries > 0) {
             printf("Pendu :\n");
 
             // Afficher le pendu à l'aide d'une boucle
-            for (int i = 0; i < game.tries; i++) {
+            for (int i = 0; i < game->tries; i++) {
                 if (i == 0) {
                     printf("  +---+\n");
                 }
@@ -107,8 +101,7 @@ void Game() {
                 }
                 else if (i >= 5) {
                     printf(" / \\  |\n");
-                }
-
+               }
             }
         }
 
@@ -116,81 +109,100 @@ void Game() {
         scanf_s(" %d", &nb);
         if (nb == 2) {
             printf("You left the game.\n");
+            Replay(game);
             return;
         }
 
         char lettre;
         scanf_s(" %c", &lettre);
 
-        if (strchr(game.lettresDevinees, lettre) != NULL) {
+        if (strchr(game->lettresDevinees, lettre) != NULL) {
             printf("You've already guessed this letter. Try again.\n");
             continue;
         }
 
-        game.lettresDevinees[strlen(game.lettresDevinees)] = lettre;
+        game->lettresDevinees[strlen(game->lettresDevinees)] = lettre;
 
-        if (strchr(game.motSecret, lettre) != NULL) {
+        if (strchr(game->motSecret, lettre) != NULL) {
             printf("Good letter!\n");
         }
         else {
             printf("The letter is not in the word.\n");
-            game.tries++;
+            game->tries++;
         }
 
         int motComplet = 1;
-        for (int i = 0; i < game.longueurMot; i++) {
-            if (strchr(game.lettresDevinees, game.motSecret[i]) == NULL) {
+        for (int i = 0; i < game->longueurMot; i++) {
+            if (strchr(game->lettresDevinees, game->motSecret[i]) == NULL) {
                 motComplet = 0;
                 break;
             }
         }
 
         if (motComplet) {
-            printf("\nCongratulations, you've guessed the word: %s\nThanks for playing the hangman's game!\n", game.motSecret);
-            break;
+            printf("\nCongratulations, you've guessed the word: %s\nThanks for playing the hangman's game!\n", game->motSecret);
+            Replay(game);
+            return;
         }
     }
 
-    if (game.tries >= MAX_TRIES) {
-        printf("\nYou've exhausted all your attempts. The secret word was: %s\n", game.motSecret);
+    if (game->tries >= MAX_TRIES) {
+        printf("\nYou've exhausted all your attempts. The secret word was: %s\n", game->motSecret);
+        Replay(game);
     }
 }
 
 void MainMenu() {
     printf("Welcome to the hangman game!\n");
 
-        printf("-----Main Menu-----\n\n");
-        printf("[1] Start the game\n");
-        printf("or\n");
-        printf("[2] Leave the game\n");
-        printf("\nChoose an option: ");
-        
+    printf("-----Main Menu-----\n\n");
+    printf("[1] Start the game\n");
+    printf("or\n");
+    printf("[2] Leave the game\n");
+    printf("\nChoose an option: ");
 
-        int MenuChoice = 2;
+    int MenuChoice = 2;
 
-        enum MenuButtons {
-            Start = 1,
-            Quit,
-        };
+    enum MenuButtons {
+        Start = 1,
+        Quit,
+    };
 
-        enum MenuButtons MyMenuButtons = MenuChoice;
-        scanf_s("%d", &MenuChoice);
+    enum MenuButtons MyMenuButtons = MenuChoice;
+    scanf_s("%d", &MenuChoice);
 
-        // Appelle à la fonction 
-
-        switch (MenuChoice)
-        {
-        case Start:
-            Game();
-            break;
-
-        case Quit:
-            ExitApp();
-            break;
-
-        default:
-            printf("Good bye!");
-            break;
-        }
+    switch (MenuChoice) {
+    case Start: {
+        struct HangmanGame game;
+        Game(&game);
+        break;
     }
+    case Quit:
+        ExitApp();
+        break;
+    default:
+        printf("Goodbye!\n");
+        break;
+    }
+}
 
+
+void Replay(struct HangmanGame* game) {
+    char choix;
+
+    printf("Do you want to replay? (O/N) : ");
+    fflush(stdin);
+    scanf_s(" %c", &choix);
+
+    if (choix == 'O' || choix == 'o') {
+        Game(&game);
+    }
+    else if (choix == 'N' || choix == 'n') {
+        printf("Thank you for playing! Goodbye.\n");
+        ExitApp();
+    }
+    else {
+        printf("Answer incorrect. Please answer by Y(yes) or N(no)\n");
+        Replay(game);
+    }
+}
